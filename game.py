3e358c337af53075ef8e39d2e1906937a34bd190
AchuZ! <83104538+AchuZ-235@users.noreply.github.com>
2022-06-20 18:40:35 +0530

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
 import random
 
 def main():
+    
+    #hint
+    
+    H = random.randint(0, 5)
+    Hint = pc - H or pc + H
+    print("Hint is.......",Hint)
+    
     number = random.randint(10, 100)
     user_input = int(input("Enter a number between 10-100: "))
+    
+    #conditions for winning or losing.
+    
     if user_input == number:
         print("You just won!")
     elif user_input > number:
@@ -16,6 +26,8 @@ def main():
     else:
         print("Number was",number)
 
+#Again        
+        
 def again():
     user_input = input("Play again?: ")
     if user_input == "yes" or "Yes":
